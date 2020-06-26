3d2505e531dc7150de28aae98c4525554c2c5265
michael_oryshchenko <michael_oryshchenko@pandats.com>
2020-06-26 17:25:46 +0300

    update

--- src/tests/Runner.py
+++ src/tests/Runner.py
@@ -1,10 +1,17 @@
 from src.tests.TestSendForm import TestSendForm
+from selenium import webdriver
 
 
 def main():
-    test = TestSendForm()
+    # create instant of browser
+    driver = webdriver.Chrome(executable_path=
+                              "C:/Users/pandaTemp/PycharmProjects/Herolo/chromedriver.exe")
+    driver.maximize_window()
+    driver.get("https://automation.herolo.co.il/")
 
-    test.test_send_form()
+    # test running
+    test = TestSendForm()
+    test.test_send_form(driver)
 
 
 if __name__ == "__main__":
--- src/tests/TestSendForm.py
+++ src/tests/TestSendForm.py
@@ -1,18 +1,11 @@
 from src.page_objects.SendFormPage import SendFormPage, SendFormConstants
-from selenium import webdriver
 
 
 class TestSendForm:
 
-    def test_send_form(self):
-        # create instant of browser
-        driver = webdriver.Chrome(executable_path=
-            "C:/Users/pandaTemp/PycharmProjects/automation-newforexqa/src/main/python/resources/grid/drivers/chromedriver2.exe")
-        driver.maximize_window()
-        driver.get("https://automation.herolo.co.il/")
-
-        # send form
+    def test_send_form(self, driver):
         send_form_page = SendFormPage(driver)
-        send_form_page.send_form(name=SendFormConstants.NAME.value,
-                                 email=SendFormConstants.EMAIL.value,
-                                 phone=SendFormConstants.PHONE.value)
+        send_form_page\
+            .send_form(name=SendFormConstants.NAME.value,
+                       email=SendFormConstants.EMAIL.value,
+                       phone=SendFormConstants.PHONE.value)
